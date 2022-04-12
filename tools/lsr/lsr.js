bb585a490f52255e5929a98201ec62c65fb176f2
David Nuescheler <david@nuescheler.ch>
2022-04-11 18:01:35 -0600

    feat: ls -R -> URLs

---
+++ tools/lsr/lsr.js
@@ -0,0 +1,40 @@
+async function traverseCurrentFolder() {
+  const URLs = [];
+  const [prefix] = window.location.href.split('/Shared%20Documents/');
+  const usp = new URLSearchParams(window.location.search);
+  const rootPath = usp.get('id').split('/Shared Documents')[1];
+
+  const traverseFolder = async (path) => {
+    const getDirEntries = async (path, type) => {
+      const apiURL = `${prefix}/_api/web/GetFolderByServerRelativeUrl('Shared%20Documents${path}')/${type}`;
+      const resp = await fetch (apiURL);
+      const xml = await resp.text();
+      const dp = new DOMParser();
+      const dom = dp.parseFromString(xml, 'text/xml');
+      const entries = [...dom.querySelectorAll('ServerRelativeUrl')];
+      return entries.map((e) => e.textContent.split('Shared Documents')[1]);
+    }
+  
+    const getFiles = async (path) => await getDirEntries(path, 'Files');
+    const getFolders = async (path) => await getDirEntries(path, 'Folders');
+    const files = await getFiles(path);
+    files.forEach((file) => {
+      let cleanPath = '';
+      if (file.endsWith('.docx')) cleanPath = file.split('.')[0].substring(rootPath.length);
+      if (file.endsWith('.xlsx')) cleanPath = file.split('.')[0].substring(rootPath.length) + '.json';
+  
+      URLs.push(`${window.domainPrefix}${cleanPath}`);
+    });
+  
+    const folders = await getFolders(path);  
+    for (let i = 0; i < folders.length; i += 1) {
+      const folder = folders[i];
+      await traverseFolder(folder);
+      console.log(URLs.length);
+    }
+  }
+  await traverseFolder(rootPath);
+  console.log(URLs.join('\n'));
+}
+
+traverseCurrentFolder();
