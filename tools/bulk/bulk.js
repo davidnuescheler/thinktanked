1af9ccc44271da13a17f2e88b26d169e661ce229
Aravindo Wingeier <synox@users.noreply.github.com>
2023-08-24 17:59:39 -0700

    Update bulk.js

--- tools/bulk/bulk.js
+++ tools/bulk/bulk.js
@@ -1,43 +1,41 @@
-const [$urls, $operation, submit, log] = [...document.querySelectorAll('.id')];
-
-console.log(log);
-
-const append = (string) => { 
-  const p = document.createElement('p'); 
-  p.textContent = string; 
-  log.append(p);
+const log = document.getElementById('logger');
+const append = (string, status="unknown") => {
+    const p = document.createElement('p');
+    p.textContent = string;
+    p.classList.add("s-"+status);
+    log.append(p);
 }
 
+document.getElementById('start').addEventListener('click', () => {
+    let counter = 0;
 
-submit.addEventListener(('click'), () => {
-  let counter = 0;
-
-  const executeOperation = async (url) => {
-    const { hostname, pathname } = new URL(url);
-    const [branch, repo, owner] = hostname.split('.')[0].split('--');
-    const adminURL = `https://admin.hlx.page/${operation}/${owner}/${repo}/${branch}${pathname}`;
-    const resp = await fetch(adminURL, {
-        method: 'POST',
-    }); 
-    const text = await resp.text();
-    console.log(text);
-    counter += 1;
-    append(`${counter}/${total}: ${adminURL}`);
-  }
-
-  const dequeue = async () => {
-    while (urls.length) {
-      const url = urls.shift();
-      await executeOperation(url);    
+    const executeOperation = async (url) => {
+        const { hostname, pathname } = new URL(url);
+        const [branch, repo, owner] = hostname.split('.')[0].split('--');
+        const adminURL = `https://admin.hlx.page/${operation}/${owner}/${repo}/${branch}${pathname}`;
+        const resp = await fetch(adminURL, {
+            method: 'POST',
+        });
+        const text = await resp.text();
+        console.log(text);
+        counter += 1;
+        append(`${counter}/${total}: ${adminURL}`, resp.status);
+        document.getElementById('total').textContent = `${counter}/${total}`;
     }
-  };
-  
-  const operation = $operation.value;
-  const concurrency = operation === 'live' ? 40 : 3;
-  const urls = $urls.value.split('\n').map(e => e.trim());
-  const total = urls.length;
-  append(`URLs: ${urls.length}`);
-  for (let i = 0; i < concurrency; i += 1) {
-    dequeue();
-  }
+
+    const dequeue = async () => {
+        while (urls.length) {
+            const url = urls.shift();
+            await executeOperation(url);
+        }
+    };
+
+    const operation = document.getElementById('select').value;
+    const concurrency = operation === 'live' ? 40 : 3;
+    const urls = document.getElementById('textarea').value.split('\n').map(e => e.trim());
+    const total = urls.length;
+    append(`URLs: ${urls.length}`);
+    for (let i = 0; i < concurrency; i += 1) {
+        dequeue();
+    }
 });
