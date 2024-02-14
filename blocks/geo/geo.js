517fade299699e82aa71169cc4c6de38a01ad2c4
David Nuescheler <david@nuescheler.ch>
2024-02-14 13:55:19 -0700

    chore: test georouting

---
+++ blocks/geo/geo.js
@@ -0,0 +1,5 @@
+import { countryInfo } from '../../geo.js';
+
+export default function decorate() {
+  console.log(countryInfo());
+}
---
+++ geo.js
@@ -0,0 +1,3 @@
+export function countryInfo() {
+    return import.meta.url;
+};
