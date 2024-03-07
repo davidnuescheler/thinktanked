0f429579eaeff6a2ae28152fed03ec04177a40fa
David Nuescheler <david@nuescheler.ch>
2024-03-07 10:09:59 -0700

    chore: initial simple filter

--- tools/rum-slicer/slicer.js
+++ tools/rum-slicer/slicer.js
@@ -2,27 +2,56 @@ let SAMPLE_BUNDLE;
 let DOMAIN_KEY = '1234';
 let DOMAIN = 'www.thinktanked.com';
 
+const filterInput = document.getElementById('filter');
+const canvas = document.getElementById('myChart');
+let dataChunks = [];
+
+// eslint-disable-next-line no-undef, no-new
+const chart = new Chart(canvas, {
+  type: 'bar',
+  data: {
+    labels: ['this'],
+    datasets: [{
+      label: 'Page Views',
+      data: [2000],
+      barPercentage: 1,
+      categoryPercentage: 0.95,
+      backgroundColor: '#7FB2F0',
+      hoverBackgroundColor: '#35478C',
+    }],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
 async function loadSampleBundle() {
   const resp = await fetch('./sampleRUMBundle.json');
   return (resp.json());
 }
 
-async function createRandomBundle() {
+async function createRandomBundle(date, hour) {
   if (!SAMPLE_BUNDLE) {
     SAMPLE_BUNDLE = await loadSampleBundle();
-    console.log(SAMPLE_BUNDLE);
   }
+  const sampleURLs = ['https://www.thinktanked.org/', 'https://www.thinktanked.org/pretzels', 'https://www.thinktanked.org/iba-testing'];
   const bundle = structuredClone(SAMPLE_BUNDLE);
   bundle.id = `${Math.random()}`;
   bundle.userAgent = Math.random() < 0.5 ? 'desktop' : 'mobile';
+  bundle.url = sampleURLs[Math.floor(Math.random() * 3)];
+  bundle.timeSlot = `${date}T${hour}:00:00Z`;
   return (bundle);
 }
 
-async function generateRandomRUMBundles(num) {
+async function generateRandomRUMBundles(num, date, hour) {
   const bundles = [];
   for (let i = 0; i < num; i += 1) {
     // eslint-disable-next-line no-await-in-loop
-    const bundle = await createRandomBundle();
+    const bundle = await createRandomBundle(date, hour);
     bundles.push(bundle);
   }
   return (bundles);
@@ -37,7 +66,7 @@ async function fetchUTCHour(utcISOString) {
   // const resp = await fetch(apiEndPoint);
   // const json = await resp.json();
   // const { rumBundles } = json;
-  const rumBundles = await generateRandomRUMBundles(Math.random() * 1000);
+  const rumBundles = await generateRandomRUMBundles(Math.random() * 100, date, hour);
   return { date, hour, rumBundles };
 }
 
@@ -47,46 +76,70 @@ export function setDomain(domain, key) {
 }
 
 export async function fetchLastWeek() {
-  const timeSeries = [];
+  const chunks = [];
   const date = new Date();
   const hoursInWeek = 7 * 24;
   for (let i = 0; i < hoursInWeek; i += 1) {
     // eslint-disable-next-line no-await-in-loop
-    timeSeries.unshift(await fetchUTCHour(date.toISOString()));
+    chunks.unshift(await fetchUTCHour(date.toISOString()));
     date.setHours(date.getHours() - 1);
   }
-  return timeSeries;
+  return chunks;
+}
+
+function filterBundle(bundle, filter) {
+  let matched = true;
+  if (!bundle.url.includes(filter.text)) {
+    matched = false;
+  }
+  return (matched);
+}
+
+function createChartData(bundles, config) {
+  const labels = [];
+  const data = [];
+  if (config.view === 'previousWeek') {
+    const hoursInWeek = 7 * 24;
+    const counts = {};
+
+    bundles.forEach((bundle) => {
+      if (!counts[bundle.timeSlot]) counts[bundle.timeSlot] = 0;
+      counts[bundle.timeSlot] += bundle.weight;
+    });
+
+    const date = new Date();
+    for (let i = 0; i < hoursInWeek; i += 1) {
+      const [dateString, time] = date.toISOString().split('T');
+      const hour = time.split(':')[0];
+      const timeSlot = `${dateString}T${hour}:00:00Z`;
+
+      labels.unshift(`${hour}:00`);
+      data.unshift(counts[timeSlot]);
+      date.setHours(date.getHours() - 1);
+    }
+  }
+
+  return { labels, data };
 }
 
 async function draw() {
-  const ctx = document.getElementById('myChart');
-
-  const timeSeries = await fetchLastWeek();
-
-  const labels = timeSeries.map((e) => `${e.hour}:00`);
-  const data = timeSeries.map((e) => e.rumBundles.length * 100);
-  // eslint-disable-next-line no-undef, no-new
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels,
-      datasets: [{
-        label: 'Page Views',
-        data,
-        barPercentage: 1,
-        categoryPercentage: 0.95,
-        backgroundColor: '#7FB2F0',
-        hoverBackgroundColor: '#35478C',
-      }],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
+  const filtered = [];
+  const filter = {
+    text: filterInput.value,
+  };
+  dataChunks.forEach((chunk) => {
+    filtered.push(...chunk.rumBundles.filter((bundle) => filterBundle(bundle, filter)));
   });
+  const { labels, data } = createChartData(filtered, { view: 'previousWeek' });
+  chart.data.datasets[0].data = data;
+  chart.data.labels = labels;
+  chart.update();
 }
 
-draw();
+async function loadData() {
+  dataChunks = await fetchLastWeek();
+  draw();
+}
+
+loadData();
+filterInput.addEventListener('input', draw);
