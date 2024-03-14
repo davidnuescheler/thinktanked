ca4d3f5a484ee5fd6e00b3616bb1c7f82b85bd60
David Nuescheler <david@nuescheler.ch>
2024-03-13 21:58:55 -0600

    chore: add unkown filetype

--- tools/lsr/lsr.js
+++ tools/lsr/lsr.js
@@ -20,20 +20,24 @@ async function traverseCurrentFolder() {
     const files = await getFiles(path);
     files.forEach((file) => {
       let cleanPath = '';
-      console.log(file);
+
       if (file.endsWith('.docx')) cleanPath = file.split('.')[0].substring(rootPath.length);
       if (file.endsWith('.xlsx')) cleanPath = file.split('.')[0].substring(rootPath.length) + '.json';
       if (file.endsWith('.pdf')) cleanPath = file.split('.')[0].substring(rootPath.length) + '.pdf';
+      if (cleanPath !== '') {
+        cleanPath = cleanPath.toLowerCase();
+        cleanPath = cleanPath.replaceAll(' ', '-');
+        cleanPath = cleanPath.replaceAll('&', '-');
+        cleanPath = cleanPath.replaceAll('\'', '-');
+        cleanPath = cleanPath.replaceAll('--', '-');
+        cleanPath = cleanPath.replaceAll('--', '-');
+        cleanPath = cleanPath.replaceAll('--', '-');
+    
+        URLs.push(`${window.domainPrefix}${cleanPath}`);  
+      } else {
+        console.log(`unkown filetype: ${file}`);
+      }
 
-      cleanPath = cleanPath.toLowerCase();
-      cleanPath = cleanPath.replaceAll(' ', '-');
-      cleanPath = cleanPath.replaceAll('&', '-');
-      cleanPath = cleanPath.replaceAll('\'', '-');
-      cleanPath = cleanPath.replaceAll('--', '-');
-      cleanPath = cleanPath.replaceAll('--', '-');
-      cleanPath = cleanPath.replaceAll('--', '-');
-  
-      URLs.push(`${window.domainPrefix}${cleanPath}`);
     });
   
     const folders = await getFolders(path);  
