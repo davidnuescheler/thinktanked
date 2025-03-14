2d144b4a4665ce0e80ca82b6402d25bc53754132
David Nuescheler <david@nuescheler.ch>
2025-03-14 13:33:51 -0600

    chore: testing

--- tools/snapshot-admin/401.js
+++ tools/snapshot-admin/401.js
@@ -1,7 +1,8 @@
-document.body.innerHTML = '<h1>401 : Unauthorized</h1>';
+window.addEventListener('DOMContentLoaded', () => {
+  document.body.innerHTML = '<h1>401 : Unauthorized</h1>';
 
-document.body.style.display = 'flex';
-document.body.style.justifyContent = 'center';
-document.body.style.alignItems = 'center';
-document.body.style.height = '100vh';
-
+  document.body.style.display = 'flex';
+  document.body.style.justifyContent = 'center';
+  document.body.style.alignItems = 'center';
+  document.body.style.height = '100vh';
+});
